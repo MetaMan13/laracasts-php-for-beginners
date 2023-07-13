01ead5ac0bb17a844cdc6943e6a6db79463073e6
MetaMan13 <imamovica1397@gmail.com>
2023-07-13 09:34:58 +0200

    Episode 38 - Register a New User

--- controllers/about.php
+++ controllers/about.php
@@ -1,7 +1,5 @@
 <?php
 
-dd($_SESSION);
-
 view("about.view.php", [
     'heading' => 'About Us',
 ]);
--- controllers/index.php
+++ controllers/index.php
@@ -1,7 +1,5 @@
 <?php
 
-$_SESSION['name'] = 'Test';
-
 view("index.view.php", [
     'heading' => 'Home',
 ]);
